5e97c766ffa59a3532e8aab4390e4c3c1fa27df3
mo2men165 <moamenabdeltawab@gmail.com>
2023-12-20 01:56:30 +0200

    .next-added

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"D:\\Courses\\FullStack\\Front-End\\React\\Websites\\Metaversus\\app\\page":["D:\\Courses\\FullStack\\Front-End\\React\\Websites\\Metaversus\\styles\\globals.css"]},"D:\\Courses\\FullStack\\Front-End\\React\\Websites\\Metaversus\\app\\layout.js":["D:\\Courses\\FullStack\\Front-End\\React\\Websites\\Metaversus\\styles\\globals.css"]}
