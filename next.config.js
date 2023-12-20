0ca07c9bb5d320e7b13051f093e246b2ebec9768
mo2men165 <moamenabdeltawab@gmail.com>
2023-12-20 02:24:25 +0200

    next-config-update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }
+    }
+  }
 };
 
 module.exports = nextConfig;
