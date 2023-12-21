a1c557d20c599a7c5b18f7000eaae6f7c5b311ca
mo2men165 <moamenabdeltawab@gmail.com>
2023-12-21 03:22:38 +0200

    fix-image-paths

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,17 +17,17 @@ const Navbar = () => (
       className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
     >
       <img
-        src="/search.svg"
+        src="./search.svg"
         alt="search"
-        className="w-[24px] h-[24px] object-contain"
+        className="w-[24px] h-[24px] object-contain cursor-pointer"
       />
       <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
         METAVERSUS
       </h2>
       <img
-        src="/menu.svg"
+        src="./menu.svg"
         alt="menu"
-        className="w-[24px] h-[24px] object-contain"
+        className="w-[24px] h-[24px] object-contain cursor-pointer"
       />
     </div>
   </motion.nav>
